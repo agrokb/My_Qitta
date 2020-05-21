adc24e6a4b41c5e53a1ee10a7f76dd46d8117199
larry <dh821020@gmail.com>
2020-05-22 02:19:45 +0900

    Boostrap fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
-import '../stylesheets';
+import '../stylesheets/application';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
