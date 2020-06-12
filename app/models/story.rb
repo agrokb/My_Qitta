43183e59f05c399cd5d001c03007f8cb2bf406d3
larry <dh821020@gmail.com>
2020-06-12 22:24:56 +0900

    URL fix

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,16 +1,13 @@
 class Story < ApplicationRecord
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :slug_candidate, use: :slugged
   belongs_to :user
   def normalize_friendly_id(input)
     input.to_s.to_ascii.to_slug.normalize(transliterations: :russian).to_s
   end
   private
   def slug_candidate
-    [
-      :title,
-      [:title,SecureRandom.hex[0,8]]
-    ]
+      SecureRandom.hex[0,8]
   end
 
 end
